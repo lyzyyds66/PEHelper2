38b62fa8f01f0effbf6c572c7658846fcc3fb7ff
yz l <1@2.fuc>
2025-01-11 13:01:42 +0800

    修改窗口大小，优化中文字体显示

--- main.c
+++ main.c
@@ -9,14 +9,17 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 #define ID_CMD_BUTTON 1
 #define ID_REGEDIT_BUTTON 2
 #define ID_REBOOT_BUTTON 3
-#define ID_DISM_BUTTON 4
-#define ID_7ZIP_BUTTON 5
-#define ID_DG_BUTTON 6
-#define ID_FM_BUTTON 7
+#define ID_DISKPART_BUTTON 4
+#define ID_FM_BUTTON 5
 
+// 声明全局变量
+HFONT hFont;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+    // 启用 DPI 感知
+    SetProcessDPIAware();
+
     // 注册窗口类
     WNDCLASS wc = { 0 };
     wc.lpszClassName = L"PEHelperClass";  // 使用宽字符字面量
@@ -36,7 +39,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         L"Windows Preinstallation Environment Helper",                    // 窗口标题
         WS_OVERLAPPEDWINDOW & ~(WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU | WS_THICKFRAME), // 窗口样式。禁止最小化，禁止最大化，禁止系统菜单（包括关闭按钮），禁止调整窗口大小
         CW_USEDEFAULT, CW_USEDEFAULT,  // 窗口初始位置
-        500, 500,                      // 窗口大小
+        550, 500,                      // 窗口大小
         NULL,                          // 父窗口句柄
         NULL,                          // 菜单句柄
         hInstance,                     // 当前实例句柄
@@ -47,6 +50,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         MessageBox(NULL, L"主窗口创建失败!", L"Error!", MB_ICONEXCLAMATION | MB_OK);  // 使用宽字符字面量
         return 0;
     }
+
+    // 创建字体
+    hFont = CreateFont(
+        -MulDiv(5, GetDeviceCaps(GetDC(hwnd), LOGPIXELSY), 72),
+        0, 0, 0,
+        FW_NORMAL, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS,
+        CLEARTYPE_QUALITY, VARIABLE_PITCH,
+        TEXT("Microsoft YaHei"));
 
     // 创建命令提示符按钮
     HWND hCmdButton = CreateWindow(
@@ -61,6 +73,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         (HMENU)ID_CMD_BUTTON,          // 按钮ID
         hInstance,                     // 实例句柄
         NULL);                         // 不使用
+    SendMessage(hCmdButton, WM_SETFONT, (WPARAM)hFont, TRUE);
 
     // 创建注册表编辑器按钮
     HWND hRegeditButton = CreateWindow(
@@ -75,6 +88,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         (HMENU)ID_REGEDIT_BUTTON,      // 按钮ID
         hInstance,                     // 实例句柄
         NULL);                         // 不使用
+    SendMessage(hRegeditButton, WM_SETFONT, (WPARAM)hFont, TRUE);
 
     //创建关机按钮（PE命令）
     HWND hPowerOffButton = CreateWindow(
@@ -89,6 +103,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         (HMENU)ID_POWEROFF_BUTTON,      // 按钮ID
         hInstance,                     // 实例句柄
         NULL);                         // 不使用
+    SendMessage(hPowerOffButton, WM_SETFONT, (WPARAM)hFont, TRUE);
 
     //创建重启按钮（PE系统下主程序推出，系统自动重启）
     HWND hRebootButton = CreateWindow(
@@ -103,46 +118,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         (HMENU)ID_REBOOT_BUTTON,      // 按钮ID
         hInstance,                     // 实例句柄
         NULL);
-    /*
-    HWND hDismButton = CreateWindow(
+    SendMessage(hRebootButton, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+
+    HWND hDiskPartButton = CreateWindow(
         L"BUTTON",                     // 预定义类；使用按钮类
-        L"Dism++",               // 按钮上的文字
+        L"DiskPart",               // 按钮上的文字
         WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // 样式
         300,                           // 位置x
         150,                           // 位置y
         100,                           // 按钮宽度
         30,                            // 按钮高度
         hwnd,                          // 父窗口
-        (HMENU)ID_DISM_BUTTON,      // 按钮ID
+        (HMENU)ID_DISKPART_BUTTON,      // 按钮ID
         hInstance,                     // 实例句柄
         NULL);
 
-    HWND h7ZipButton = CreateWindow(
-        L"BUTTON",                     // 预定义类；使用按钮类
-        L"7-Zip",               // 按钮上的文字
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // 样式
-        100,                           // 位置x
-        150,                           // 位置y
-        100,                           // 按钮宽度
-        30,                            // 按钮高度
-        hwnd,                          // 父窗口
-        (HMENU)ID_7ZIP_BUTTON,      // 按钮ID
-        hInstance,                     // 实例句柄
-        NULL);
 
-    HWND hDGButton = CreateWindow(
-        L"BUTTON",                     // 预定义类；使用按钮类
-        L"DiskGenius",               // 按钮上的文字
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // 样式
-        100,                           // 位置x
-        200,                           // 位置y
-        100,                           // 按钮宽度
-        30,                            // 按钮高度
-        hwnd,                          // 父窗口
-        (HMENU)ID_DG_BUTTON,      // 按钮ID
-        hInstance,                     // 实例句柄
-        NULL);
-        */
     HWND hFMButton = CreateWindow(
         L"BUTTON",                     // 预定义类；使用按钮类
         L"简易文件管理器",               // 按钮上的文字
@@ -155,14 +147,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         (HMENU)ID_FM_BUTTON,      // 按钮ID
         hInstance,                     // 实例句柄
         NULL);
-
-    // 检查按钮是否创建成功(弃用)
-    /*
-    if (hCmdButton == NULL || hRegeditButton == NULL || hPowerOffButton == NULL || hRebootButton == NULL)
-    {
-        MessageBox(hwnd, L"按钮创建失败！", L"错误", MB_ICONERROR | MB_OK);  // 使用宽字符字面量
-        return 0;
-    }*/
+    SendMessage(hFMButton, WM_SETFONT, (WPARAM)hFont, TRUE);
 
     // 显示窗口
     ShowWindow(hwnd, nShowCmd);
@@ -175,6 +160,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    // 删除字体对象
+    DeleteObject(hFont);
 
     return msg.wParam;
 }
@@ -201,31 +189,21 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         // 执行pe关机指令
         else if (LOWORD(wParam) == ID_POWEROFF_BUTTON)
         {
-            ShellExecute(hwnd, L"open", L"wpeutil", L"shutdown", NULL, SW_SHOW);  // 父窗口句柄，要执行的操作，要执行的程序（可带路径），传参（例如 /c），工作目录，窗口显示方式（SW_SHOW，SW_HIDE等）
+            // 父窗口句柄，要执行的操作，要执行的程序（可带路径），传参（例如 /c），工作目录，窗口显示方式（SW_SHOW，SW_HIDE等）
+            ShellExecute(hwnd, L"open", L"wpeutil", L"shutdown", NULL, SW_SHOW);
         }
-        // 直接推出程序，等待系统自动重启
+        // 直接退出程序，等待系统自动重启
         else if (LOWORD(wParam) == ID_REBOOT_BUTTON)
         {
-            PostQuitMessage(0);  
-        }
-        /*
-        // 打开Dism++
-        else if (LOWORD(wParam) == ID_DISM_BUTTON)
-        {
-            ShellExecute(hwnd, L"open", L"X:\\SWWORK_DIR\\SOFTWARE\\Dism++10.1.1002.1\\Dism++x64.exe", NULL, NULL, SW_SHOW);  // 使用宽字符字面量
+            PostQuitMessage(0);
         }
 
-        // 7zip
-        else if (LOWORD(wParam) == ID_7ZIP_BUTTON)
+        // 打开Dism++
+        else if (LOWORD(wParam) == ID_DISKPART_BUTTON)
         {
-            ShellExecute(hwnd, L"open", L"X:\\SWWORK_DIR\\SOFTWARE\\7-Zip\\7zFM.exe", NULL, NULL, SW_SHOW);  // 使用宽字符字面量
+            ShellExecute(hwnd, L"open", L"diskpart.exe", NULL, NULL, SW_SHOW);  // 使用宽字符字面量
         }
-        // dg
-        else if (LOWORD(wParam) == ID_DG_BUTTON)
-        {
-            ShellExecute(hwnd, L"open", L"X:\\SWWORK_DIR\\SOFTWARE\\DiskGenius\\DiskGenius.exe", NULL, NULL, SW_SHOW);  // 使用宽字符字面量
-        }
-        */
+
         // file manager
         else if (LOWORD(wParam) == ID_FM_BUTTON)
         {
@@ -248,13 +226,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             {
                 // 用户选择了文件，szFile 中包含文件路径
                 MessageBox(hwnd, szFile, L"Selected File", MB_OK);  // 显示选择的文件路径
-                MessageBox(hwnd, L"本功能的用意是让您在精简版pe系统无explorer的情况下通过系统内置函数弹出一个文件管理器页面，在此页面您可以右键单击某程序并运行。\n本程序的运行不需三方运行库，您无需向本程序传递文件参数。", L"关于本功能的说明", MB_OK);
+                MessageBox(hwnd, L"本功能的用意是让您在精简版pe系统无explorer的情况下通过系统内置函数弹出一个文件管理器页面，在此页面您可以右键单击某程序并运行。", L"关于本功能的说明", MB_OK);
+                MessageBox(hwnd, L"本程序的运行不需三方运行库，无需向本程序传递文件参数。", L"关于本功能的说明", MB_OK);
             }
         }
         return 0;
 
-
-        // 因为程序设计之初去除了右上角关闭按钮，所以这个case暂时用不上了
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
